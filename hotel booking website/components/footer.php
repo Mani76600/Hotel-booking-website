bb8d987d638ff5b4758b7e97f3c168c656189a62
Jeereddy Mani chenna Kesava reddy <139867170+Mani76600@users.noreply.github.com>
2024-09-28 12:42:00 +0530

    Add files via upload

---
+++ hotel booking website/components/footer.php
@@ -0,0 +1,36 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:7660039792"><i class="fas fa-phone"></i> +91 7660039792</a>
+         <a href="tel:7036223939"><i class="fas fa-phone"></i> +91 7036223939</a>
+         <a href="mailto:manichennakesava@gmail.com"><i class="fas fa-envelope"></i> manichennakesava@gmail.com</a>
+         <a href="mailto:mahanth@gmail.com"><i class="fas fa-envelope"></i> mahanth@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Woxsen University- Hyderabad 502345</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="https://www.instagram.com/_mani_chenna_kesava_reddy/?next=%2F">instagram <i class="fab fa-instagram"></i></a>
+         <a href="https://www.linkedin.com/in/jeereddy-mani-chenna-kesava-reddy-92a62b229/">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="https://www.youtube.com/channel/UCOb3ZMDaM9e-asl2M7bsDtA">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2023  by Mani, Mahanth | all rights reseved!</div>
+
+</section>
+
